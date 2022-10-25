bbd1beeac25fb6b0bd2d1335d6e42c0c5eef729e
Edna Maria Farias Moreira <edna.maria887@gmail.com>
2022-10-24 23:59:47 -0300

    Myportifólio - page home

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const home = document.querySelector('.home')
+const about = document.querySelector('.about')
+const myProjects = document.querySelector('.projects')
+const contact = document.querySelector('.contact')
+
+
+
+  home.addEventListener('click', () => {
+    home.classList.add('active')
+
+    about.classList.remove('active')
+    myProjects.classList.remove('active')
+    contact.classList.remove('active')
+  })
+
+
+  about.addEventListener('click', () => {
+    about.classList.add('active')
+
+    home.classList.remove('active')
+    myProjects.classList.remove('active')
+    contact.classList.remove('active')
+  })
+
+  myProjects.addEventListener('click', () => {
+    myProjects.classList.add('active')
+
+    home.classList.remove('active')
+    about.classList.remove('active')
+    contact.classList.remove('active')
+  })
+
+  contact.addEventListener('click', () => {
+    contact.classList.add('active')
+
+    home.classList.remove('active')
+    about.classList.remove('active')
+    myProjects.classList.remove('active')
+  })
---
+++ js/variables.js
@@ -0,0 +1,62 @@
+// const home = document.querySelector('li .home')
+// const about = document.querySelector('li .about')
+// const myProjects = document.querySelector('li .projects')
+// const contact = document.querySelector('li .contact')
+
+
+
+// function pages() {
+//   home.addEventListener('onclick', () => {
+//     home.classList.add('active')
+
+//     about.classList.remove('active')
+//     myProjects.classList.remove('active')
+//     contact.classList.remove('active')
+//   })
+
+
+//   about.addEventListener('onclick', () => {
+//     about.classList.add('active')
+
+//     home.classList.remove('active')
+//     myProjects.classList.remove('active')
+//     contact.classList.remove('active')
+//   })
+
+//   myProjects.addEventListener('onclick', () => {
+//     myProjects.classList.add('active')
+
+//     home.classList.remove('active')
+//     about.classList.remove('active')
+//     contact.classList.remove('active')
+//   })
+
+//   contact.addEventListener('onclick', () => {
+//     contact.classList.add('active')
+
+//     home.classList.remove('active')
+//     about.classList.remove('active')
+//     myProjects.classList.remove('active')
+//   })
+
+
+
+
+
+
+
+
+
+
+// }
+
+
+
+
+
+// export {
+//   home,
+//   about,
+//   myProjects,
+//   contact
+// }
